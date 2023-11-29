b6f03c384daaded637c64e79fd87216a92fa5d5a
Vishwanauth Persaud <vpersaud1245@gmail.com>
2023-11-29 18:17:36 -0500

    Add createBookCardElement function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const libraryGrid = document.querySelector(".library-grid");
 const library = [];
 
 function Book(title, author, pages, isRead) {
@@ -17,6 +18,51 @@ function displayBooks() {
   });
 }
 
+function createBookCardElement(book) {
+  const bookCard = document.createElement("div");
+  bookCard.className = "book-card";
+  const bookInfo = document.createElement("div");
+  bookInfo.className = "book-info";
+  bookCard.appendChild(bookInfo);
+  const bookTitle = document.createElement("div");
+  bookTitle.className = "book-title";
+  bookTitle.textContent = book.title;
+  bookInfo.appendChild(bookTitle);
+  const numOfPages = document.createElement("div");
+  numOfPages.className = "num-of-pages";
+  numOfPages.textContent = `Pages: ${book.pages}`;
+  bookInfo.appendChild(numOfPages);
+  const author = document.createElement("div");
+  author.className = "author";
+  author.textContent = `by ${book.author}`;
+  bookInfo.appendChild(author);
+  bookCard.appendChild(document.createElement("hr"));
+  const bookCardButtons = document.createElement("div");
+  bookCardButtons.className = "book-card-buttons";
+  const changeReadStatusButton = document.createElement("button");
+  const progressInfo = document.createElement("div");
+  progressInfo.className = "progress-info";
+  changeReadStatusButton.className = "change-read-status";
+  if (book.isRead) {
+    changeReadStatusButton.textContent = "Read";
+    progressInfo.textContent = "Completed";
+  } else {
+    changeReadStatusButton.textContent = "Not Read";
+    progressInfo.textContent = "In Progress";
+  }
+  bookCardButtons.appendChild(changeReadStatusButton);
+  const deleteBookButton = document.createElement("button");
+  deleteBookButton.className = "delete-book";
+  deleteBookButton.textContent = "Delete";
+  bookCardButtons.appendChild(deleteBookButton);
+  bookCard.appendChild(bookCardButtons);
+  const bottomBreak = document.createElement("hr");
+  bottomBreak.className = "bottom-break";
+  bookCard.appendChild(bottomBreak);
+  bookCard.appendChild(progressInfo);
+  libraryGrid.appendChild(bookCard);
+}
+
 addBookToLibrary(new Book("Harry Potter", "J.K Rowling", 344, true));
 addBookToLibrary(new Book("Lord of the Rings", "J.J Tolkein", 546, false));
 addBookToLibrary(new Book("Atomic Habits", "James Clear", 214, false));
@@ -30,3 +76,4 @@ addBookToLibrary(
 );
 
 displayBooks();
+createBookCardElement(new Book("Lord of the Rings", "J.J Tolkein", 546, true));
